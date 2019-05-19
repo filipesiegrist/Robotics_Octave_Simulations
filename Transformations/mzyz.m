0a4cd4d9f81031797df44a23f545fcbedaa64dd1
filipesiegrist <filipebrasil8@gmail.com>
2019-05-19 17:24:15 -0300

    Adding the RPY and ZYZ transformations.

---
+++ Transformations/mzyz.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2019 Filipe-UFSC
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} mzyz (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Filipe-UFSC <filipe-ufsc@filipeufsc-Aspire-5740>
+## Created: 2019-04-05
+
+function out_matrix = zyz( alfa, beta, gama )
+%Rotaciona o in_point gama em torno de x, beta em torno de y e alfa em
+%torno de x. ANGULOS EM RADIANOS!
+addpath('../Basic_Transformations/');
+out_matrix = mrotz(alfa)*mroty(beta)*mrotz(gama)
+
+end
