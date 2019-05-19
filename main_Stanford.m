4fea59d5549ee2b22f7905a63c2dab2e48e662d8
filipesiegrist <filipebrasil8@gmail.com>
2019-05-19 18:54:32 -0300

    Starting to simulate the Stanford manipulator.

--- main_Stanford.m
+++ main_Stanford.m
@@ -9,33 +9,41 @@ addpath('./Differential_Kinematics/');
 addpath('./Denavit_Hartenberg/');
 
 %Variaveis simbolicas
-syms a_2;
-syms a_3;
+syms d_2;
+syms d_3;
+syms d_6;
 syms theta_1;
 syms theta_2;
-syms theta_3;
-syms x;
-syms y;
-syms z;
+syms theta_4;
+syms theta_5;
+syms theta_6;
 
 %Tabela dos parametros DH
 %ATENÇÃO: Essa tabela segue o padrão apresentado no livro do Craig (ver exemplos 3.3 e 3.4), com a(i-1) e alfa(i-1) nas linhas.
-DH_Antropomorfico = [
+DH_Stanford = [
 	[0 0 0 theta_1]
-	[pi/2 0 0 theta_2]
-	[0 a_2 0 theta_3]
-	[0 a_3 0 0]
+	[-pi/2 0 d_2 theta_2]
+	[pi/2 0 d_3 0]
+	%[0 0 0 theta_4]
+	%[-pi/2 0 0 theta_5]
+	%[pi/2 0 d_6 theta_6]
 ];
+
+% Mapeamento dos tipos de juntas
+ROTATIVA = 0;
+PRISMATICA = 1;
+
+Juntas_Antropomorfico = [ROTATIVA, ROTATIVA, PRISMATICA, ROTATIVA, ROTATIVA, ROTATIVA];
 
 syms joint_z joint_p;
 
 % Parametros das juntas:
-[joint_z, joint_p] = jointParameters(DH_Antropomorfico)
+[joint_z, joint_p] = jointParameters(DH_Stanford, Juntas_Antropomorfico)
 
 % jacobiano da cinemática
-S = jacobianMatrix(joint_z, joint_p)
+%Jc = jacobianMatrix(joint_z, joint_p)
 
 % jacobiano da estática
-T = transpose(S)
+%Je = transpose(Jc)
 
 clear;
