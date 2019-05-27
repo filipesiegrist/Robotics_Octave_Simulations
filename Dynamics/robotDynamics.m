32290144bd00e905d2299a64513b66c783a67832
filipesiegrist <filipebrasil8@gmail.com>
2019-05-27 20:20:28 -0300

    Creating a good model for the fors.

--- Dynamics/robotDynamics.m
+++ Dynamics/robotDynamics.m
@@ -25,10 +25,9 @@
 #	
 
 function [joint_forces, joint_torques] = robotDynamics(N, z_vector, pos_vector, rotation_matrices, joint_speeds, joint_accelerations, link_masses, link_inertia_tensors, link_mass_centers, link_x)
-
-	assert(1 == 2, "Leaving the function.");
-	%links = 0:N-1;
-	%joints = (N):1;
+	N = 3;
+	links = 0:(N-1);
+	joints = N:1;
 
 	% "Outer" iterations
 
@@ -42,7 +41,7 @@ function [joint_forces, joint_torques] = robotDynamics(N, z_vector, pos_vector, 
 	F_i_plus_1 = [];
 	N_i_plus_1 = [];
 
-	for link=2:links:
+	for link=links
 		%Do the stuff
 	end
 
@@ -52,7 +51,7 @@ function [joint_forces, joint_torques] = robotDynamics(N, z_vector, pos_vector, 
 	f_i = []; %Take the last value at the F_i_plus_1
 	n_i = []; %Take the last value at the n_i_plus_1
 
-	for joint=joints:
+	for joint=joints
 		%Do the stuff
 	end
 
