83e3af7db9259e6200492188eed5cf5f4f2fa56a
filipesiegrist <filipebrasil8@gmail.com>
2019-05-27 20:49:08 -0300

    Adding a file do do my own calculations to test the functions.

---
+++ main_teste.m
@@ -0,0 +1,43 @@
+% Robô analisado: Planar 2R
+
+clear;
+warning('off','all');
+
+% Importações
+addpath('./Inverse_Kinematics/');
+addpath('./Differential_Kinematics/');
+addpath('./Denavit_Hartenberg/');
+addpath('./Dynamics/');
+
+%Variaveis simbolicas
+syms a_1;
+syms a_2;
+syms theta_1;
+syms theta_2;
+
+%Tabela dos parametros DH
+%ATENÇÃO: Essa tabela segue o padrão apresentado no livro do Craig (ver exemplos 3.3 e 3.4), com a(i-1) e alfa(i-1) nas linhas.
+DH_2R = [
+	[0 0 0 theta_1]
+	[0 a_1 0 theta_2]
+	[0 a_2 0 0]
+];
+
+syms joint_z joint_p joint_r joint_rp;
+
+% Parametros dos elos:
+[joint_r, joint_rp] = linkParameters(DH_2R)
+
+% Parametros das juntas:
+[joint_z, joint_p] = jointParameters(DH_2R);
+
+% jacobiano da cinemática
+Jc = jacobianMatrix(joint_z, joint_p)
+
+% jacobiano da estática
+Je = transpose(Jc)
+
+% calculo da dinamica:
+%robotDynamics()
+
+clear;
