249e20f136a3358dc923da75289fa81265de222c
filipesiegrist <filipebrasil8@gmail.com>
2019-05-31 16:47:27 -0300

    Prepared the text main to use when creating the dynamics calculation.

--- main_teste.m
+++ main_teste.m
@@ -26,18 +26,43 @@ DH_2R = [
 syms joint_z joint_p joint_r joint_rp;
 
 % Parametros dos elos:
-[joint_r, joint_rp] = linkParameters(DH_2R)
+[joint_r, joint_rp] = linkParameters(DH_2R);
 
 % Parametros das juntas:
 [joint_z, joint_p] = jointParameters(DH_2R);
 
 % jacobiano da cinemática
-Jc = jacobianMatrix(joint_z, joint_p)
+%Jc = jacobianMatrix(joint_z, joint_p)
 
 % jacobiano da estática
-Je = transpose(Jc)
+%Je = transpose(Jc)
 
 % calculo da dinamica:
-%robotDynamics()
+
+%Numero de elos
+N_2R = 2;
+
+% Velocidades e aceleracoes angulares
+syms speed_joint_1 speed_joint_2;
+syms acc_joint_1 acc_joint_2;
+speeds_2R = [speed_joint_1; speed_joint_2];
+acc_2R = [acc_joint_1; acc_joint_2];
+
+% Os centros de massa estão nas pontas.
+mass_centers_2R = joint_rp(:, 2:3);
+syms m_1 m_2;
+masses_2R = [m_1; m_2];
+
+
+% Tensores de inercia (considerados como zero.)
+I_tensor_2R = zeros(3,6);
+
+% Remover a primeira parte dos parametros:
+joint_r = joint_r(:, 4:9)
+joint_rp = joint_rp(:, 2:3);
+joint_z = joint_z(:, 2:3);
+
+
+[joint_F, joint_T] = robotDynamics(N_2R, joint_z, joint_rp, joint_r, speeds_2R, acc_2R, masses_2R, I_tensor_2R, mass_centers_2R);
 
 clear;
