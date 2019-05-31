d5248008ed63f67b26aef3fe65638b65cd63769a
filipesiegrist <filipebrasil8@gmail.com>
2019-05-31 19:26:04 -0300

    Changing the X position and the Mass center to be the correct, and simpler, way.

--- main_teste.m
+++ main_teste.m
@@ -49,7 +49,12 @@ speeds_2R = [speed_joint_1; speed_joint_2];
 acc_2R = [acc_joint_1; acc_joint_2];
 
 % Os centros de massa estão nas pontas.
-mass_centers_2R = joint_rp(:, 2:3); %MUDAR para a1*x1 e a2*x2
+mass_centers_2R = [
+	[a_1 a_2]
+	[ 0   0 ]
+	[ 0   0 ]
+] %MUDAR para a1*x1 e a2*x2
+
 syms m_1 m_2;
 masses_2R = [m_1; m_2];
 
@@ -65,6 +70,13 @@ joint_z = joint_z(:, 2:3);
 % Aceleracao no efetuador:
 syms g;
 initial_acceleration_2R = [0; g ; 0];
+
+%Teste alternativo para os joint points. FUNCIONOU
+joint_rp = [
+	[0 a_1 a_2]
+	[0  0   0 ]
+	[0  0   0 ]
+]
 
 [joint_F, joint_T] = robotDynamics(N_2R, joint_z, joint_rp, joint_r, speeds_2R, acc_2R, masses_2R, I_tensor_2R, mass_centers_2R, initial_acceleration_2R);
 
