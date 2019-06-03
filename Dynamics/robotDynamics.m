ac0b92b2e20d4034617e4050a5669b08aac67856
filipesiegrist <filipebrasil8@gmail.com>
2019-06-03 14:33:58 -0300

    Adding the equation 6.50

--- Dynamics/robotDynamics.m
+++ Dynamics/robotDynamics.m
@@ -29,6 +29,7 @@ function [joint_forces, joint_torques] = robotDynamics(N, z_vector, relative_x_v
 	links = 1:N;
 	joints = N:1;
 
+
 	% Initial conditions
 	% The link zero has no angular velocity.
 	omega_i = [0; 0; 0];
@@ -49,6 +50,7 @@ function [joint_forces, joint_torques] = robotDynamics(N, z_vector, relative_x_v
 
 	for link=links
 		rot_mtrx_cols = ((link-1)*3+1):((link-1)*3+3);
+		inertia_tensor_cols = ((link-1)*3+1):((link-1)*3+3);
 		% Declaring the used parameters
 		R_i_1 = transpose(rotation_matrices(:, rot_mtrx_cols));  % It needs to be transposed because we inverted the references
 		dot_theta_i = joint_speeds(link);
@@ -57,29 +59,32 @@ function [joint_forces, joint_torques] = robotDynamics(N, z_vector, relative_x_v
 		P_i_1 = relative_x_vector(:, link);
 		Pc_i_1 = link_mass_centers(:, link);
 		m_i_1 = link_masses(link);
+		I_i_1 = link_inertia_tensors(:, inertia_tensor_cols);
 
-		%Eq. 6,45
+		%Eq. 6.45
 		omega_i_1 = R_i_1*omega_i + dot_theta_i*Z_i_1;
 		omega_i_1 = simplify(omega_i_1)
 
-		%Eq. 6,46
+		%Eq. 6.46
 		dot_omega_i_1 = R_i_1*dot_omega_i + cross(R_i_1*omega_i, dot_theta_i*Z_i_1) + dot_dot_theta_i*Z_i_1;
 		dot_omega_i_1 = simplify(dot_omega_i_1)
 		
-		%Eq. 6,47
+		%Eq. 6.47
 		dot_v_i = R_i_1*(cross(dot_omega_i, P_i_1) + cross(omega_i, cross(omega_i, P_i_1)) + dot_v_i);
 		dot_v_i = simplify(dot_v_i)
 
-		%Eq. 6,48
+		%Eq. 6.48
 		dot_vc_i = cross(dot_omega_i_1, Pc_i_1) + cross(omega_i_1, cross(omega_i_1, Pc_i_1)) + dot_v_i;
 		dot_vc_i = simplify(dot_vc_i)
 
-		%Eq. 6,49
+		%Eq. 6.49
 		F_i_1 = m_i_1*dot_vc_i;
 		F_i_1 = simplify(F_i_1)
 
-		%Eq. 6,50
-	
+		%Eq. 6.50
+		N_i_plus_1 = I_i_1*dot_omega_i_1 + cross(omega_i_1, (I_i_1*omega_i_1));
+		N_i_plus_1 = simplify(N_i_plus_1)
+
 		%Updating the omega_i
 		omega_i = omega_i_1;
 		dot_omega_i = dot_omega_i_1;
